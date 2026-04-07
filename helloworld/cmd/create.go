cf12810ad299b64ff7a4abe4ea511783f7d3c81a
Lucidixp <alucidi90@gmail.com>
2026-04-07 16:31:02 +0000

    inital project push. nothing fancy mostly boilerplate code

---
+++ helloworld/cmd/create.go
@@ -0,0 +1,34 @@
+package cmd
+
+import {
+    "github.com/charmbracelet/lipgloss"
+    "github.com/spf13/cobra"
+}
+
+var {
+   logoStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("#A01FAC6")).Bold(true)
+   tipMsgStyle = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color("190")).Italic(true)
+   endingMsgStyle = lipgloss.NewStyle().PaddingLeft(1).Foreground(lipgloss.Color("170")).Bold(true)
+}
+
+
+func init() {
+  rootCmd.AddCommand(createCmd)
+}
+
+type listOptions struct {
+   options []string
+}
+
+type Options struct {
+    ProjectName string
+    ProjectType string
+}
+var createCmd = &cobra.Command{
+    Use: "create",
+    Short: "short description",
+    Long: ".",
+
+    Run: func(cmd *.cobra.Command, args []string) {
+  },
+}
---
+++ helloworld/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "example/helloworld/cmd"
+
+func main() {
+	cmd.Execute()
+}
